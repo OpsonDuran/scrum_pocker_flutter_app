ef2d98f8b634d1a846c0841510583d7d7e1d78c5
OpsonDuran <iccarmandoduran@gmail.com>
2019-07-24 02:05:33 -0500

    update ValueCard to StatefullWidget and add flag for view value of card

--- lib/src/value_card.dart
+++ lib/src/value_card.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 
 
-class ValueCard extends StatelessWidget {
+class ValueCard extends StatefulWidget {
 
+  @override
+  _ValueCardState createState() => _ValueCardState();
+}
+
+class _ValueCardState extends State<ValueCard> {
+  bool flag = false;
   @override
   Widget build(BuildContext context) {
 
     final String item = ModalRoute.of(context).settings.arguments;
+    
     return Scaffold(
-        body: Container(
-          color: Colors.blue,  
-          child: Center(
-            child: Text(item,style: TextStyle(color: Colors.white,fontSize: 170.0,fontWeight: FontWeight.bold),)),
+        body: GestureDetector(
+          onTap: (){
+            print(flag);
+            if(flag==false){
+            flag=true;
+            setState(()=>flag);
+            }else{
+            flag=false;
+            setState(()=>flag);
+            }
+          },
+            child: Container(
+            color: Colors.blue,  
+            child: Center(
+            child: _value(flag,item)),
       ),
+        ),
     );
+  }
+
+  Widget _value(bool flag,String item){
+    if(flag){
+      return Text(item,style: TextStyle(color: Colors.white,fontSize: 170.0,fontWeight: FontWeight.bold),);
+    }else{
+      return Container(color: Colors.blueAccent,);
+    }
   }
 }
