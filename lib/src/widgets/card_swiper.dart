169c91d3bac3892766f308fffc9b01b01bca1420
OpsonDuran <iccarmandoduran@gmail.com>
2019-07-24 00:38:02 -0500

    integrate items in swiper

--- lib/src/widgets/card_swiper.dart
+++ lib/src/widgets/card_swiper.dart
@@ -2,27 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 class CardSwiper extends StatelessWidget {
-
   final List<dynamic> items;
 
   CardSwiper({@required this.items});
 
   @override
   Widget build(BuildContext context) {
-
     final _screenSizeContext = MediaQuery.of(context).size;
 
     return Container(
       child: Swiper(
-          itemBuilder: (BuildContext context,int index){
-            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-          },
-          itemCount: 3,
-          layout: SwiperLayout.STACK,
-          itemWidth:  _screenSizeContext.width * 0.7,
-          itemHeight: _screenSizeContext.height * 0.75,
-        ),
+        itemBuilder: (BuildContext context, int index) {
+          return ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Container(
+                  color: Colors.blueAccent,
+                  child: Center(
+                      child: Text(
+                    items[index].toString(),
+                    style: TextStyle(
+                        fontSize: 150.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ))));
+        },
+        itemCount: items.length,
+        layout: SwiperLayout.STACK,
+        itemWidth: _screenSizeContext.width * 0.8,
+        itemHeight: _screenSizeContext.height * 0.7,
+      ),
     );
   }
 }
-
