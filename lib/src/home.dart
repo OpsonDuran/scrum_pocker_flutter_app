c5a5607ffcb8b6acd478e6614b998d02eee2d0ee
OpsonDuran <iccarmandoduran@gmail.com>
2019-07-23 23:34:50 -0500

    add appBar with title and icons

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -4,6 +4,16 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.star),
+            Text('Scrum Pocker Ninja'),
+            Icon(Icons.star)
+          ],
+        ),
+      ),
       body: SafeArea(child: Text('Hola Mundo')),
     );
   }
